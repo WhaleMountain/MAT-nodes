993022652481c465a057208fcf616df03f79326c
Asakura613 <e195716@ie.u-ryukyu.ac.jp>
2019-08-24 09:55:15 +0900

    first commit

---
+++ unity_sample/Assets/Resources/Script/CameraScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CameraScript : MonoBehaviour
+{
+    private const float YAngle_MIN = -89.0f;   //カメラのY方向の最小角度
+    private const float YAngle_MAX = 89.0f;     //カメラのY方向の最大角度
+    public Transform target;    //追跡するオブジェクトのtransform
+    public Vector3 offset;      //追跡対象の中心位置調整用オフセット
+    private Vector3 lookAt;     //targetとoffsetによる注視する座標
+    private float distance = 10.0f;    //キャラクターとカメラ間の角度
+    private float distance_min = 1.0f;  //キャラクターとの最小距離
+    private float distance_max = 20.0f; //キャラクターとの最大距離
+    private float currentX = 0.0f;  //カメラをX方向に回転させる角度
+    private float currentY = 0.0f;  //カメラをY方向に回転させる角度
+                                    //カメラ回転用係数(値が大きいほど回転速度が上がる)
+    private float moveX = 4.0f;     //マウスドラッグによるカメラX方向回転係数
+    private float moveY = 2.0f;     //マウスドラッグによるカメラY方向回転係数
+    private float moveX_QE = 2.0f;  //QEキーによるカメラX方向回転係数
+    void Start()
+    {
+    }
+    void Update()
+    {
+        //QとEキーでカメラ回転
+        if (Input.GetKey(KeyCode.Q))
+        {
+            currentX += -moveX_QE;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            currentX += moveX_QE;
+        }
+        //マウス右クリックを押しているときだけマウスの移動量に応じてカメラが回転
+        if (Input.GetMouseButton(1))
+        {
+            currentX += Input.GetAxis("Mouse X") * moveX;
+            currentY += Input.GetAxis("Mouse Y") * moveY;
+            currentY = Mathf.Clamp(currentY, YAngle_MIN, YAngle_MAX);
+        }
+        distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel"), distance_min, distance_max);
+    }
+    void LateUpdate()
+    {
+        if (target != null)  //targetが指定されるまでのエラー回避
+        {
+            lookAt = target.position + offset;  //注視座標はtarget位置+offsetの座標
+                                                //カメラ旋回処理
+            Vector3 dir = new Vector3(0, 0, -distance);
+            Quaternion rotation = Quaternion.Euler(-currentY, currentX, 0);
+            transform.position = lookAt + rotation * dir;   //カメラの位置を変更
+            transform.LookAt(lookAt);   //カメラをLookAtの方向に向けさせる
+        }
+    }
+}
---
+++ unity_sample/Assets/Resources/Script/LauncherScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LauncherScript : Photon.PunBehaviour
+{
+    #region Public変数定義
+    //Public変数の定義はココで
+    #endregion
+    #region Private変数
+    //Private変数の定義はココで
+    string _gameVersion = "test";   //ゲームのバージョン。仕様が異なるバージョンとなったときはバージョンを変更しないとエラーが発生する。
+    #endregion
+    #region Public Methods
+    //ログインボタンを押したときに実行される
+    public void Connect()
+    {
+        if (!PhotonNetwork.connected)
+        {                         //Photonに接続できていなければ
+            PhotonNetwork.ConnectUsingSettings(_gameVersion);   //Photonに接続する
+            Debug.Log("Photonに接続しました。");
+        }
+    }
+    #endregion
+    #region Photonコールバック
+    //Auto-JoinLobbyにチェックを入れているとPhotonに接続後OnJoinLobby()が呼ばれる。
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("ロビーに入りました。");
+        //Randomで部屋を選び、部屋に入る（部屋が無ければOnPhotonRandomJoinFailedが呼ばれる）
+        PhotonNetwork.JoinRandomRoom();
+    }
+    //JoinRandomRoomが失敗したときに呼ばれる
+    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+    {
+        Debug.Log("ルームの入室に失敗しました。");
+        //TestRoomという名前の部屋を作成して、部屋に入る
+        PhotonNetwork.CreateRoom("TestRoom");
+    }
+    //部屋に入った時に呼ばれる
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("ルームに入りました。");
+        //battleシーンをロード
+        PhotonNetwork.LoadLevel("battle");
+    }
+    #endregion
+}
